10e0f38445ae526266534f4e05d595ddd54fd785
ritikgupta90x <rg131589@gmail.com>
2025-11-10 15:04:25 -0800

    Change cookie settings for token management
    Updated cookie settings to use 'None' for sameSite and enabled secure flag.

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -26,8 +26,8 @@ export const signUp = async (req, res) => {
     // 🍪 Set cookie properly
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax", // localhost par "lax"
-      secure: false,   // only true in production (https)
+      sameSite: "None", // localhost par "lax"
+      secure:true,   // only true in production (https)
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -62,8 +62,8 @@ export const login = async (req, res) => {
     // 🍪 Set cookie properly
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
-      secure: false,
+       sameSite: "None", // localhost par "lax"
+      secure:true, 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
